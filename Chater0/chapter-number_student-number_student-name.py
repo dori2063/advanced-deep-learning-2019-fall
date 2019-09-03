7d7c4b124501ca9b28550b9d204375c303593e3f
Kyoobin Lee <kyoobinlee@gmail.com>
2019-09-03 09:36:08 +0900

    0.0.0

---
+++ Chater0/chapter-number_student-number_student-name.py
@@ -0,0 +1,29 @@
+"""
+Base code: chapter 0.0.0 listing 0.1 ~ 0.5
+
+Name: Gildong Hong
+
+Modification:
+(English description is required)
+I replaced 'hello world' by 'hello advanced deep learning'
+
+(Korean description can be added for better understanding)
+'hello world' 를 'hello advanced deep learning' 으로 바꾸어 보았다.
+
+Result:
+C:\Users\kyoob\Anaconda3\envs\DeepLearningWithPython\python.exe C:/Users/kyoob/PycharmProjects/advanced-deep-learning-2019-fall/chapter-number_student-number_student-name.py
+hello advanced deep learning
+
+Process finished with exit code 0
+
+
+Discussion:
+(English description is required)
+'world' is replaced by 'advanced deep learning'
+
+(Korean description can be added for better understanding)
+'advanced deep learning' 부분이 'world'로 바뀌었다.
+
+"""
+
+print('hello advanced deep learning')
